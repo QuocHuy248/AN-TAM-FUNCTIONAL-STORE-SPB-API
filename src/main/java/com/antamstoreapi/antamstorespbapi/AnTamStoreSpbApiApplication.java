256279d5d7923d95cae01533407caea5c7adcffb
QuocHuy248 <quochuy248@gmail.com>
2024-03-20 10:43:01 +0700

    first commit

---
+++ src/main/java/com/antamstoreapi/antamstorespbapi/AnTamStoreSpbApiApplication.java
@@ -0,0 +1,13 @@
+package com.antamstoreapi.antamstorespbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnTamStoreSpbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnTamStoreSpbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antamstoreapi/antamstorespbapi/AnTamStoreSpbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antamstoreapi.antamstorespbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnTamStoreSpbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
